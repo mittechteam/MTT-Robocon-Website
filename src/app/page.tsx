3bdb392bb4b215360a81105e8dd528d215cd46f1
Mehul Uttam <mehulsavio@gmail.com>
2024-10-13 14:14:41 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-
 import dynamic from "next/dynamic";
 import Carousel from "@/components/logoCarousel/carousel";
 import Footer from "@/components/footer/footer";
+import GradualSpacing from "@/components/ui/gradual-spacing";
+import { Instagram, Linkedin, Twitter, YoutubeIcon } from "lucide-react";
 
 const Scene = dynamic(() => import("@/components/model/scene"), { ssr: false });
+
+const socialIcons = [
+  { Icon: Instagram, url: "https://www.instagram.com/robocon.mit/", hoverColor: "hover:text-pink-500" },
+  { Icon: Linkedin, url: "https://www.linkedin.com/company/mittechteam/", hoverColor: "hover:text-blue-500" },
+  { Icon: Twitter, url: "https://x.com/MITtechTeam", hoverColor: "hover:text-white" },
+  { Icon: YoutubeIcon, url: "https://www.youtube.com/c/MITRoboconTechTeam", hoverColor: "hover:text-red-500" },
+];
 
 export default function Page() {
   return (
@@ -18,14 +26,30 @@ export default function Page() {
             At MIT-World Peace University, Pune, India, we are proud
             participants in the prestigious ABU Robocon event each year. With
             over 100 Indian teams competing annually, our team stands out as one
-            of the top robotics teams in the country. As we set our sights on the
-            DD Robocon 2025, we are more determined than ever to achieve victory
-            and once again proudly represent India on the international stage.
-            Join us on this exciting journey!
+            of the top robotics teams in the country. As we set our sights on
+            the DD Robocon 2025, we are more determined than ever to achieve
+            victory and once again proudly represent India on the international
+            stage. Join us on this exciting journey!
           </p>
-          <p className="mt-4 font-normal text-base text-justify text-neutral-300">Social Logos</p>
+          <div className="mr-9">
+            <GradualSpacing
+              className="font-display text-start text-lg font-bold -tracking-widest text-[#c73808] dark:text-white md:text-xl md:leading-[5rem]"
+              text="We don't build Robots, We build people who build robots."
+            />
+          </div>
+          <div className="flex gap-x-6">
+            {socialIcons.map(({ Icon, url, hoverColor }, index) => (
+              <a 
+                key={index} 
+                href={url} 
+                className={`text-gray-400 ${hoverColor} transition-colors duration-300`}
+              >
+                <Icon size={30} />
+              </a>
+            ))}
+          </div>
         </div>
-        <div className="w-full md:w-1/2 h-[50vh] md:h-[80vh] flex items-center justify-center">
+        <div className="w-full md:w-1/2 h-[50vh] md:h-[70vh] flex items-center justify-center">
           <Scene />
         </div>
       </div>
--- src/components/logoCarousel/carousel.tsx
+++ src/components/logoCarousel/carousel.tsx
@@ -1,50 +1,49 @@
 import React from "react";
 import Image from "next/image";
 
-import logo1 from "@/assets/logos/sponsor-1.png";
-import logo2 from "@/assets/logos/sponsor-2.png";
-import logo3 from "@/assets/logos/sponsor-3.png";
-import logo4 from "@/assets/logos/sponsor-4.png";
-import logo5 from "@/assets/logos/sponsor-5.png";
-import logo6 from "@/assets/logos/sponsor-6.png";
-import logo7 from "@/assets/logos/sponsor-7.png";
+import logo1 from "@/assets/logos/sponsor-1.svg";
+import logo2 from "@/assets/logos/sponsor-2.svg";
+import logo3 from "@/assets/logos/sponsor-31.svg";
+import logo4 from "@/assets/logos/sponsor-4.svg";
+import logo5 from "@/assets/logos/sponsor-5.svg";
+import logo6 from "@/assets/logos/sponsor-61.svg";
+import logo7 from "@/assets/logos/sponsor-7.svg";
 
-
-const logos = [logo1,logo2,logo3,logo4,logo5,logo6,logo7];
+const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7];
 
 const Carousel: React.FC = () => {
   return (
     <div className="min-w-full pb-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h3 className="text-5xl text-center font-semibold text-white mb-4">
-          Our Sponsors
+          Our <span className="text-[#c73808]">Sponsors</span>
         </h3>
-        <p className="text-base font-normal text-center text-neutral-300 mb-8">
-          Our SponsorOur SponsorOur SponsorOur SponsorOur SponsorOur Sponsor
+        <p className="px-10 text-base font-normal text-center text-neutral-300 mb-8">
+          our valued sponsors who make our projects and initiatives possible.
+          Their support drives innovation and empowers us to create meaningful
+          impact. Thank you for being a key part of our journey!
         </p>
         <div className="flex overflow-hidden select-none">
-          <div className="flex items-center justify-around whitespace-nowrap w-full animate-marquee">
+          <div className="flex shrink-0 items-center justify-around whitespace-nowrap animate-marquee">
             {logos.concat(logos).map((logo, index) => (
               <div key={index} className="mx-4">
                 <Image
                   src={logo}
                   alt={`Partner logo ${index + 1}`}
-                  width={100}
-                  height={100}
-             
+                  width={200}
+                  height={200}
                 />
               </div>
             ))}
           </div>
-          <div className="flex shrink-0 items-center justify-around whitespace-nowrap w-full animate-marquee">
+          <div className="flex shrink-0 items-center justify-around whitespace-nowrap animate-marquee">
             {logos.concat(logos).map((logo, index) => (
               <div key={index + logos.length} className="mx-4">
                 <Image
                   src={logo}
                   alt={`Partner logo ${index + 1}`}
-                  width={100}
-                  height={100}
-                  className="object-contain"
+                  width={200}
+                  height={200}
                 />
               </div>
             ))}
