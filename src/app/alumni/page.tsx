37d6ac4de882aed8d5bc66e57cb63c293dc5ea54
Mehul Uttam <mehulsavio@gmail.com>
2024-12-19 16:02:22 +0530

    Update package dependencies and enhance alumni page content

--- src/app/alumni/page.tsx
+++ src/app/alumni/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="min-h-screen w-full">
+      <div className=" py-40 w-full">
+        <div className="max-w-7xl mx-auto text-center">
+          <p className="font-bold text-xl md:text-4xl text-white">
+            Our <span className="text-red-500">Alumni</span>
+          </p>
+          <p className="text-base text-neutral-300 max-w-6xl text-justify mx-auto py-4">
+            Our alumni have gone on to achieve remarkable success in various
+            fields across the globe. From pioneering technological advancements
+            to leading major corporations, they continue to make significant
+            contributions to society. Their journey with our club has equipped
+            them with the skills and experiences necessary to excel in their
+            respective careers. We are proud of their accomplishments and the
+            positive impact they are making in the world.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
